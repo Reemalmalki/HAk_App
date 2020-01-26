3ffbb0cb189da9b5bdbc004f638387ebb3b77885
Reemalmalki <55227438+Reemalmalki@users.noreply.github.com>
2020-01-26 12:44:31 +0300

    screens

---
+++ HAK_V1/ViewControllers/signUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  signUpViewController.swift
+//  HAK_V1
+//
+//  Created by Reem Almalki on 01/06/1441 AH.
+//  Copyright © 1441 Reem Almalki. All rights reserved.
+//
+
+import UIKit
+
+class signUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
