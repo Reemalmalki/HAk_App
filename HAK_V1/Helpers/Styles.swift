3730ef6748a787796fe42731232bb34488ea01f8
Reemalmalki <55227438+Reemalmalki@users.noreply.github.com>
2020-02-02 10:27:20 +0300

    1

---
+++ HAK_V1/Helpers/Styles.swift
@@ -0,0 +1,58 @@
+//
+//  Styles.swift
+//  HAK_V1
+//
+//  Created by Reem Almalki on 03/06/1441 AH.
+//  Copyright © 1441 Reem Almalki. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+class Styles {
+    
+    static func styleTextField(_ textfield:UITextField) {
+        /*
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
+        
+        bottomLine.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1).cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)*/
+        
+    }
+    
+    static func styleMainButton(_ button:UIButton) {
+        /*
+        
+        // Filled rounded corner style
+        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.white
+ */
+    }
+    
+    static func styleButton(_ button:UIButton) {
+        /*
+        // Hollow rounded corner style
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 25.0
+        button.tintColor = UIColor.black
+ */
+    }
+    /*
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }*/
+    
+}
