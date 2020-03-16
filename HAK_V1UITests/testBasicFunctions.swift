2b7a8b5170cb754c221a8ce7fe98b89de6263626
Reemalmalki <55227438+Reemalmalki@users.noreply.github.com>
2020-03-16 19:54:06 +0300

    test 3

--- HAK_V1UITests/testBasicFunctions.swift
+++ HAK_V1UITests/testBasicFunctions.swift
@@ -1,20 +1,23 @@
 //
-//  HAK_V1UITests.swift
+//  testBasicFunctions.swift
 //  HAK_V1UITests
 //
-//  Created by Reem Almalki on 01/06/1441 AH.
+//  Created by Reem Almalki on 21/07/1441 AH.
 //  Copyright © 1441 Reem Almalki. All rights reserved.
 //
 
 import XCTest
-
-class HAK_V1UITests: XCTestCase {
+@testable import HAK_V1
+class testBasicFunctions: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -24,20 +27,8 @@ class HAK_V1UITests: XCTestCase {
     }
 
     func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
