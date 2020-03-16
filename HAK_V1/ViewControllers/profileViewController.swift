c627e96d19ee66f92f09797ff305a62d7148fec0
Reemalmalki <55227438+Reemalmalki@users.noreply.github.com>
2020-03-17 02:35:32 +0300

    with profile

--- HAK_V1/ViewControllers/profileViewController.swift
+++ HAK_V1/ViewControllers/profileViewController.swift
@@ -21,12 +21,12 @@ class profileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-getProfileData()
+        self.getProfileData()
         // Do any additional setup after loading the view.
     }
     
     func getProfileData(){
-        let ref = Database.database().reference().child("teachers").child(!self.teacherId)
+        let ref = Database.database().reference().child("teachers").child(self.teacherId!)
                ref.observeSingleEvent(of: DataEventType.value) { (DataSnapshot) in
                    let value = DataSnapshot.value as? NSDictionary
                    self.nameLable.text = value?["name"] as? String
@@ -37,12 +37,99 @@ getProfileData()
     
     
     @IBAction func editName(_ sender: Any) {
+        let alert = UIAlertController(title: "تعديل الاسم", message:"ادخل الاسم الجديد ثم اضغط على تأكيد", preferredStyle: .alert)
+               alert.addTextField { (textField) in
+                   textField.placeholder = "الاسم"
+               }
+               alert.addAction(UIAlertAction(title: "إلغاء", style: .destructive, handler: nil))
+               alert.addAction(UIAlertAction(title: "تأكيد", style: .default, handler: { [weak alert] (_) in
+                  let newName = (alert?.textFields![0].text)!
+                if newName.elementsEqual("")  {
+                   // error
+                    let alert = UIAlertController(title:"لم يتم التعديل", message: "تأكد من الكتابة بشكل صحيح", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "حسناً", style: .default, handler: nil))
+                     self.present(alert, animated: true, completion: nil)
+                    
+                }else{ // edit
+                  let ref = Database.database().reference().child("teachers").child(self.teacherId!)
+                    ref.updateChildValues(["name" : newName])
+                    self.nameLable.text = newName
+                }
+        }))
+        self.present(alert, animated: true, completion: nil)
+
     }
     
     @IBAction func editEmail(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "تعديل البريد الإلكتروني", message:"ادخل البريد الإلكتروني الجديد ثم اضغط على تأكيد", preferredStyle: .alert)
+               alert.addTextField { (textField) in
+                   textField.placeholder = "******@****"
+               }
+               alert.addAction(UIAlertAction(title: "إلغاء", style: .destructive, handler: nil))
+               alert.addAction(UIAlertAction(title: "تأكيد", style: .default, handler: { [weak alert] (_) in
+                  let newEmail = (alert?.textFields![0].text)!
+                if newEmail.elementsEqual("")  {
+                   // error
+                    let alert = UIAlertController(title:"لم يتم التعديل", message: "تأكد من الكتابة بشكل صحيح", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "حسناً", style: .default, handler: nil))
+                     self.present(alert, animated: true, completion: nil)
+                    
+                }else{ // edit
+                    Auth.auth().currentUser?.updateEmail(to: newEmail) { (error) in
+                     
+                        if error != nil { // error
+                            let alert = UIAlertController(title:"لم يتم التعديل", message: "تأكد من الكتابة بشكل صحيح", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "حسناً", style: .default, handler: nil))
+                             self.present(alert, animated: true, completion: nil)
+                            
+                        }else{
+                            let ref = Database.database().reference().child("teachers").child(self.teacherId!)
+                                               ref.updateChildValues(["name" : newEmail])
+                                               self.emailLable.text = newEmail
+                        }
+                        
+                    } }
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
     
     @IBAction func editPassword(_ sender: Any) {
+               let alert = UIAlertController(title: "تعديل كلمة المرور", message:"ادخل كلمة المرور الجديدة ثم اضغط على تأكيد", preferredStyle: .alert)
+                      alert.addTextField { (textField) in
+                          textField.placeholder = "*********"
+                      }
+                      alert.addAction(UIAlertAction(title: "إلغاء", style: .destructive, handler: nil))
+                      alert.addAction(UIAlertAction(title: "تأكيد", style: .default, handler: { [weak alert] (_) in
+                         let newPass = (alert?.textFields![0].text)!
+                       if newPass.elementsEqual("")  {
+                          // error
+                           let alert = UIAlertController(title:"لم يتم التعديل", message: "تأكد من الكتابة بشكل صحيح", preferredStyle: .alert)
+                           alert.addAction(UIAlertAction(title: "حسناً", style: .default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                           
+                       }else{ // edit
+                           Auth.auth().currentUser?.updatePassword(to: newPass) { (error) in
+                            
+                               if error != nil { // error
+                                   let alert = UIAlertController(title:"لم يتم التعديل", message: "تأكد من الكتابة بشكل صحيح", preferredStyle: .alert)
+                                   alert.addAction(UIAlertAction(title: "حسناً", style: .default, handler: nil))
+                                    self.present(alert, animated: true, completion: nil)
+                                   
+                               }else{
+                                let alert = UIAlertController(title:"تم التعديل", message: "يمكنك استخدام كلمة المرور الجديدة عند تسجيل الدخول مرة اخرى", preferredStyle: .alert)
+                                alert.addAction(UIAlertAction(title: "حسناً", style: .default, handler: nil))
+                                 self.present(alert, animated: true, completion: nil)
+                                
+                            }
+                           } }
+               }))
+               self.present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        
     }
     
 }
